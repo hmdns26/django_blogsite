de6b4a03670ca81fd028027f11b5125aa65b1aab
hmdns26 <hmd996hh@gmail.com>
2022-06-22 13:57:46 +0430

    initial commit

---
+++ blog/models.py
@@ -0,0 +1,21 @@
+from django.shortcuts import reverse
+from django.db import models
+
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('pub', 'published'),
+        ('drf', 'draft'),
+    )
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=3)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("posts_detail", args=[self.id])
